307eb505ad9adeaa3ebea39de85da224952d4c5a
Gialac2006 <gialacnguyentran@gmail.com>
2025-08-22 15:38:50 +0700

    commit

--- public/config.php
+++ public/config.php
@@ -3,7 +3,7 @@
 
 // Thông tin kết nối MySQL
 $servername = 'localhost';
-$username   = 'root';
-$password   = '';           // nếu bạn để trống mật khẩu mặc định
-$database   = 'qlvtxoai';   // thay bằng tên database của bạn
+$username   = 'sql_nhom14_itimi';
+$password   = '4aa4a6e3687378';           // nếu bạn để trống mật khẩu mặc định
+$database   = 'sql_nhom14_itimi';   // thay bằng tên database của bạn
 $port       = 3307;         // port MySQL của XAMPP
